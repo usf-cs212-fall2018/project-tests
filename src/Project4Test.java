acaec3852d67bec678f1a776fde1074041304d3b
Sophie Engle <sjengle@cs.usfca.edu>
2018-11-04 19:08:48 -0800

    Cutting down runtime of project 4 tests.

--- src/Project4Test.java
+++ src/Project4Test.java
@@ -3,16 +3,6 @@ import org.junit.jupiter.api.Nested;
 public class Project4Test {
 
 	@Nested
-	public class Project4IndexTest extends IndexTest {
-
-	}
-
-	@Nested
-	public class Project4SearchTest extends SearchTest {
-
-	}
-
-	@Nested
 	public class Project4ThreadTest extends ThreadTest {
 
 	}
